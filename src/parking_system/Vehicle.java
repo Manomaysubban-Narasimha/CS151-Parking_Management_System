dc93b263c4545d3abacf41cd3210e631781e6962
Manomaysubban-Narasimha <78935157+Manomaysubban-Narasimha@users.noreply.github.com>
2022-11-13 16:17:59 -0800

    Update Vehicle.java

--- src/parking_system/Vehicle.java
+++ src/parking_system/Vehicle.java
@@ -1,12 +1,33 @@
 package parking_system;
 
-public class Vehicle {
-	private String licenseNumber;
-	private ParkingLotOptions.VehicleOptions vehicleType;
-	
-	public Vehicle(ParkingLotOptions.VehicleOptions vehicleType, String licenseNumber) {
-		this.licenseNumber = licenseNumber;
-		this.vehicleType = vehicleType;
-	}
+public abstract class Vehicle
+{
+    private String licenseNumber;
+    private final VehicleType type;
+    private ParkingTicket ticket;
 
+    public Vehicle(VehicleType type)
+    {
+        this.type = type;
+    }
+
+    public void assignTicket(ParkingTicket ticket)
+    {
+        this.ticket = ticket;
+    }
+
+    public VehicleType getType()
+    {
+        return type;
+    }
+
+    public String getLicenseNumber()
+    {
+        return licenseNumber;
+    }
+
+    public ParkingTicket getTicket()
+    {
+        return ticket;
+    }
 }
