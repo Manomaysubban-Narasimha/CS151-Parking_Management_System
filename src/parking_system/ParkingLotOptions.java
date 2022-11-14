486bb32aa968a145bc7301d23756a3285f292ad1
Manomaysubban-Narasimha <78935157+Manomaysubban-Narasimha@users.noreply.github.com>
2022-11-13 16:20:53 -0800

    Delete ParkingLotOptions.java

--- src/parking_system/ParkingLotOptions.java
+++
@@ -1,8 +0,0 @@
-package parking_system;
-
-public class ParkingLotOptions {
-	public enum SpotOptions { EV, COMPACT, NORMAL, MOTORCYCLE, HANDICAPPED, SHORTTERM, EMPLOYEE}
-	public enum VehicleOptions {MOTORCYCLE, EV, TRUCK, CAR, SUV}
-	public enum TicketStatusOptions {LOST, ACTIVE, PAID}
-	public enum AccountStatusOptions {ACTIVE, INACTIVE, FLAGGED}
-}
