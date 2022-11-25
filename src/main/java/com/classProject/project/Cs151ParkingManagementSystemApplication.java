39cf6b35029c00c51d4fb8eede5da8d2b1693b70
akshtshrma24 <akshitsharmaa65@gmail.com>
2022-11-24 19:18:13 -0800

    Initial Commit

---
+++ src/main/java/com/classProject/project/Cs151ParkingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.classProject.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs151ParkingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs151ParkingManagementSystemApplication.class, args);
+	}
+
+}
