6bcfeaf030a282cbfac003ebbacf96205d2668e8
akshtshrma24 <akshitsharmaa65@gmail.com>
2022-11-27 20:47:27 -0800

    Switch to maven

---
+++ src/test/java/com/project/CS151Parking_Management_System/Cs151ParkingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CS151Parking_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs151ParkingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
