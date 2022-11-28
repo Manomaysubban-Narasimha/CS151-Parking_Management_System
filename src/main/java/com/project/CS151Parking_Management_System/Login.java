cc6cb5f1f803245cdd1325aef69b97ae021146a3
Manomaysubban-Narasimha <78935157+Manomaysubban-Narasimha@users.noreply.github.com>
2022-11-28 14:25:37 -0800

    Modified Password field to mask characters

--- src/main/java/com/project/CS151Parking_Management_System/Login.java
+++ src/main/java/com/project/CS151Parking_Management_System/Login.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -35,7 +36,7 @@ public class Login extends VerticalLayout{
 
         
 
-        TextField password = new TextField();
+        PasswordField password = new PasswordField();
         Paragraph passwordLabel = new Paragraph("Password");
         HorizontalLayout l3 = new HorizontalLayout(password, passwordLabel);
         l3.setAlignItems(Alignment.CENTER);
